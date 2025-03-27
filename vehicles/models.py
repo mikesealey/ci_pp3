6ea60a0edce88e86cacc87ecc906000fe9ab4a62
mikesealey <mikesealey@hotmail.com>
2025-03-27 20:10:38 +0000

    adds vehicles model

--- vehicles/models.py
+++ vehicles/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Vehicle(models.Model):
+    vrn = models.CharField(max_length=10, unique=True)
+    make = models.CharField(max_length=25, unique=False)
+    model = models.CharField(max_length=25, unique=False)
+    engineCapacity = models.CharField(max_length=25, unique=False)
+    fuelType = models.CharField(max_length=25, unique=False)
+    yearOfManufacture = models.CharField(max_length=4, unique=False)
+    colour = models.CharField(max_length=15, unique=False)
+    owner = models.ForeignKey(
+        User,
+        related_name="vehicles",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        )
+    
+# on_delete=models.SET_NULL will _not_ delete the vehicle history if the owner deletes their account. 
+# This will be important if a vehicle is sold to a different user.
+# null=true and blank=true allow empty or null values in these cases
