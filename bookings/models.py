810282b509a569adff80a7fd710ec03c09aebc10
mikesealey <mikesealey@hotmail.com>
2025-03-27 20:28:22 +0000

    adds booking model

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+from vehicles.models import Vehicle
 
 # Create your models here.
+class Booking(models.Model):
+    SERVICE = "Service"
+    MOT = "MOT"
+    REPAIR = "Repair"
+    OTHER = "Other"
+
+    BOOKING_TYPE_CHOICES = [
+        (SERVICE, "Service"),
+        (MOT, "MOT"),
+        (REPAIR, "Repair"),
+        (OTHER, "Other"),
+    ]
+
+    date_time = models.DateTimeField()
+    approved = models.BooleanField(default=False)
+    booking_type = models.CharField(max_length=10, choices=BOOKING_TYPE_CHOICES)
+    customer_notes = models.TextField(max_length=1000)
+    mechanics_notes = models.TextField(max_length=1000)
+    vehicle_mileage_at_service = models.PositiveIntegerField()
+    completed_service = models.BooleanField(default=False)
+    vehicle = models.ForeignKey(
+        Vehicle,
+        on_delete=models.CASCADE,
+        related_name="bookings"
+    )
