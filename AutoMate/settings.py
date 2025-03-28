cae7d40b8f01fb6f19b8da5a56d87c03bf4be962
mikesealey <mikesealey@hotmail.com>
2025-03-28 21:03:21 +0000

    fixes file structure to recognise templates

--- AutoMate/settings.py
+++ AutoMate/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'AutoMate.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["AutoMate/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
