eed7674cf2cc1d904e8780647e7c52a460b78c3d
mikesealey <mikesealey@hotmail.com>
2025-04-04 21:46:07 +0100

    builds left block of vehicles

--- static/js/vehicles.js
+++ static/js/vehicles.js
@@ -1,7 +1,36 @@
 $(document).ready(function () {
+    // User clicks on vehicle in list
     $(".vehicle-list-item").on("click", function () {
-        console.log('here');
-    });
-});
+        const $vehicle = $(this);
 
-console.log("HERE!")
+        const vehicleData = {
+            vrn: $vehicle.data("vrn"),
+            make: $vehicle.data("make"),
+            model: $vehicle.data("model"),
+            engine_capacity: $vehicle.data("engine_capacity"),
+            fuel_type: $vehicle.data("fuel_type"),
+            year: $vehicle.data("year"),
+            colour: $vehicle.data("colour"),
+            owner: $vehicle.data("owner"),
+        };
+
+        console.log(vehicleData);  // 🔍 check output
+
+        // Update left panel
+        $("#vrn-view").text(vehicleData.vrn);
+        $("#make-view").text(vehicleData.make);
+        $("#model-view").text(vehicleData.model);
+        $("#engine-capacity-view").text(vehicleData.engine_capacity)
+        $("#fuel-type-view").text(vehicleData.fuel_type);
+        $("#year-of-manufacture-view").text(vehicleData.year)
+        
+    });
+
+    // User clicks "Add new vehicle"
+    $("#add-new-vehicle").on("click", function() {
+        console.log("Adding new vehicle!")
+        $("#existing-vehicle").text("hidden")
+        $("#existing-vehicle").addClass(".hidden")
+        
+    })
+});
