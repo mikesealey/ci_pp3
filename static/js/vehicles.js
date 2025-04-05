e6d83fea2af681846a0ef07e187e4b4f830d4555
mikesealey <mikesealey@hotmail.com>
2025-04-05 21:50:39 +0100

    adds form to vehicles

--- static/js/vehicles.js
+++ static/js/vehicles.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     $(".vehicle-list-item").on("click", function () {
         const $vehicle = $(this);
 
+        // Get vehicle data
         const vehicleData = {
             vrn: $vehicle.data("vrn"),
             make: $vehicle.data("make"),
@@ -13,24 +14,54 @@ $(document).ready(function () {
             colour: $vehicle.data("colour"),
             owner: $vehicle.data("owner"),
         };
-
-        console.log(vehicleData);  // 🔍 check output
-
-        // Update left panel
-        $("#vrn-view").text(vehicleData.vrn);
-        $("#make-view").text(vehicleData.make);
-        $("#model-view").text(vehicleData.model);
-        $("#engine-capacity-view").text(vehicleData.engine_capacity)
-        $("#fuel-type-view").text(vehicleData.fuel_type);
-        $("#year-of-manufacture-view").text(vehicleData.year)
-        
+        // Check vehicle data
+        console.log(vehicleData);
+        // Display vehicle data
+        $("#left-block").text("") // Empyt out the placeholder text
+        $("#left-block").append($("<div>", { id: "vrn-view", class: "vrn", text: vehicleData.vrn }))
+        if (vehicleData.fuel_type === "electric") {
+            $("#vrn-view").addClass("electric")
+        }
+        $("#left-block").append($("<div>", { id: "make-view", text: vehicleData.make }));
+        $("#left-block").append($("<div>", { id: "model-view", text: vehicleData.model }));
+        $("#left-block").append($("<div>", { id: "engine-capacity-view", text: vehicleData.engine_capacity }));
+        $("#left-block").append($("<div>", { id: "fuel-type-view", text: vehicleData.fuel_type }));
+        $("#left-block").append($("<div>", { id: "year-of-manufacture-view", text: vehicleData.year }));
+        $("#left-block").append($("<div>", { id: "colour-view", text: vehicleData.colour }));
     });
 
     // User clicks "Add new vehicle"
     $("#add-new-vehicle").on("click", function() {
         console.log("Adding new vehicle!")
-        $("#existing-vehicle").text("hidden")
-        $("#existing-vehicle").addClass(".hidden")
+        // Remove displayed vehicle data
+        $("#left-block").empty()
+        $("#left-block").append($("<form>", { id: "new-vehicle-form" }));
         
+        $("#new-vehicle-form").append(
+            $("<label>", { for: "vrn", text: "VRN:" }),
+            $("<input>", { type: "text", id: "vrn", name: "vrn" }),
+            $("<br>"),
+            $("<label>", { for: "make", text: "Make:" }),
+            $("<input>", { type: "text", id: "make", name: "make" }),
+            $("<br>"),
+            $("<label>", { for: "model", text: "Model:" }),
+            $("<input>", { type: "text", id: "model", name: "model" }),
+            $("<br>"),
+            $("<label>", { for: "engine_capacity", text: "Engine Capacity:" }),
+            $("<input>", { type: "text", id: "engine_capacity", name: "engine_capacity" }),
+            $("<br>"),
+            $("<label>", { for: "fuel_type", text: "Fuel Type:" }),
+            $("<input>", { type: "text", id: "fuel_type", name: "fuel_type" }),
+            $("<br>"),
+            $("<label>", { for: "year", text: "Year:" }),
+            $("<input>", { type: "text", id: "year", name: "year" }),
+            $("<br>"),
+            $("<label>", { for: "colour", text: "Colour:" }),
+            $("<input>", { type: "text", id: "colour", name: "colour" }),
+            $("<br>"),
+          );
+        console.log($("#new-vehicle-form").children())
+        
+
     })
 });
