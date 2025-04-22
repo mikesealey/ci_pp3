d4b8ada383bf6afd91adf7869bf77ea11dd5bc5c
mikesealey <mikesealey@hotmail.com>
2025-04-22 13:13:30 +0100

    adds first/last names to profile

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,15 +3,18 @@ from django import forms
 from .models import Profile
 
 class CustomSignupForm(SignupForm):
+    first_name = forms.CharField(max_length=30, required=True, label='First name')
+    last_name = forms.CharField(max_length=30, required=True, label='Last name')
     phone_number = forms.CharField(max_length=20, required=True, label='Mobile number')
 
 
     def save(self, request):
-        # Saves the user
         user = super().save(request)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.save()
 
-        # then attatches their phone number
-        profile = user.profile  # thanks to our post_save signal this exists
+        profile = user.profile
         profile.phone_number = self.cleaned_data['phone_number']
         profile.save()
         return user
@@ -21,5 +24,5 @@ class ProfileForm(forms.ModelForm):
         model = Profile
         fields = ['phone_number']
         widgets = {
-            'phone_number': forms.TextInput(attrs={'placeholder': 'Mobile number'}),
+            'phone_number': forms.TextInput(attrs={'placeholder': 'Mobile number', 'required': 'required'}),
         }
