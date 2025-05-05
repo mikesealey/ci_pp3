23bae33ed678481726659ee82d46a23c4136b7f0
mikesealey <mikesealey@hotmail.com>
2025-05-05 10:21:42 +0100

    formatting document

--- static/js/common.js
+++ static/js/common.js
@@ -1,13 +1,12 @@
 // Trigger toast notifications
-function showToastNotification(title, message){
-    $('.toast .toast-header strong').text(title);
-    $('.toast .toast-body').text(message);
-  
-    // Show the toast
-    $('.toast').toast({ delay: 15000 });
-    $('.toast').toast('show');
-  }
+function showToastNotification(title, message) {
+  $(".toast .toast-header strong").text(title);
+  $(".toast .toast-body").text(message);
 
+  // Show the toast
+  $(".toast").toast({ delay: 15000 });
+  $(".toast").toast("show");
+}
 
 // Should move Multi-Purpose Modal function here, too.
 /**
@@ -19,7 +18,7 @@ function showToastNotification(title, message){
  *  body
  *  confirmButtonText
  *  cancelButtonText
- * 
+ *
  * }
  */
 
@@ -32,21 +31,22 @@ function showToastNotification(title, message){
  * @property {string} confirmButtonText - Text for the confirmation button.
  * @property {string} cancelButtonText - Text for the cancellation button.
  * @property {object} booking - the bookingData object
- * 
+ *
  * Opens and handles the Multi-Purpose Modal.
  * @param {ModalObject} modalObject - The configuration for the modal.
  */
 function showMultiPurposeModal(modalObject){
   console.log("Opening MultiPurposeModal")
-        $("#multi-purpose-modal-title").text(modalObject.title)
-        $("#multi-purpose-modal-body").text(modalObject.body)
-        $("#multi-purpose-modal-positive").html(modalObject.confirmButtonText)
-        $("#multi-purpose-modal-negative").html(modalObject.cancelButtonText)
-        $("#multi-purpose-modal").modal("show");
-        $("#multi-purpose-modal-positive").on("click", function () {
-          console.log("invoking function!")
-          modalObject.confirmationButtonFunction(modalObject.confirmationButtonFunctionArgument)
+  $("#multi-purpose-modal-title").text(modalObject.title)
+  $("#multi-purpose-modal-body").text(modalObject.body)
+  $("#multi-purpose-modal-positive").html(modalObject.confirmButtonText)
+  $("#multi-purpose-modal-negative").html(modalObject.cancelButtonText)
+  $("#multi-purpose-modal").modal("show");
+  $("#multi-purpose-modal-positive").on("click", function () {
+    console.log("invoking function!")
+    modalObject.confirmationButtonFunction(
+      modalObject.confirmationButtonFunctionArgument
+    )
   })
   console.log("|abandingonig service")
-  
 }
